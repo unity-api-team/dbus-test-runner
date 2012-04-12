12ac3920d196e965e520f25b88b0e9fed16f8c3e
Ted Gould <ted@gould.cx>
2012-04-12 16:02:34 -0500

    Adding prototypes for the dbus task object

--- libdbustest/task.h
+++ libdbustest/task.h
@@ -26,7 +26,36 @@ struct _DbusTestTask {
 	DbusTestTaskPrivate * priv;
 };
 
+typedef enum _DbusTestTaskState DbusTestTaskState;
+enum _DbusTestTaskState {
+	DBUS_TEST_TASK_STATE_INIT,
+	DBUS_TEST_TASK_STATE_WAITING,
+	DBUS_TEST_TASK_STATE_RUNNING,
+	DBUS_TEST_TASK_STATE_FINISHED
+};
+
+typedef enum _DbusTestTaskReturn DbusTestTaskReturn;
+enum _DbusTestTaskReturn {
+	DBUS_TEST_TASK_RETURN_NORMAL,
+	DBUS_TEST_TASK_RETURN_IGNORE,
+	DBUS_TEST_TASK_RETURN_INVERT
+};
+
 GType dbus_test_task_get_type (void);
+DbusTestTask * dbus_test_task_new (void);
+
+void dbus_test_task_set_name (DbusTestTask * task, const gchar * name);
+void dbus_test_task_set_name_spacing (DbusTestTask * task, guint chars);
+void dbus_test_task_set_wait_for (DbusTestTask * task, const gchar * dbus_name);
+void dbus_test_task_set_return (DbusTestTask * task, DbusTestTaskReturn ret);
+
+void dbus_test_task_print (DbusTestTask * task, const gchar * message);
+
+DbusTestTaskState dbus_test_task_get_state (DbusTestTask * task);
+
+void dbus_test_task_run (DbusTestTask * task);
+
+gboolean dbus_test_task_passed (DbusTestTask * task);
 
 G_END_DECLS
 
