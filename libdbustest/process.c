bfc08eb406cc24a938b37e65d8f4278e0069f730
Ted Gould <ted@gould.cx>
2012-04-13 14:48:19 -0500

    Run baby run

--- libdbustest/process.c
+++ libdbustest/process.c
@@ -20,6 +20,7 @@ static void dbus_test_process_class_init (DbusTestProcessClass *klass);
 static void dbus_test_process_init       (DbusTestProcess *self);
 static void dbus_test_process_dispose    (GObject *object);
 static void dbus_test_process_finalize   (GObject *object);
+static void process_run                  (DbusTestTask * task);
 
 G_DEFINE_TYPE (DbusTestProcess, dbus_test_process, DBUS_TEST_TYPE_TASK);
 
@@ -32,6 +33,10 @@ dbus_test_process_class_init (DbusTestProcessClass *klass)
 
 	object_class->dispose = dbus_test_process_dispose;
 	object_class->finalize = dbus_test_process_finalize;
+
+	DbusTestTaskClass * task_class = DBUS_TEST_TASK_CLASS(klass);
+
+	task_class->run = process_run;
 
 	return;
 }
@@ -62,5 +67,73 @@ dbus_test_process_finalize (GObject *object)
 
 
 	G_OBJECT_CLASS (dbus_test_process_parent_class)->finalize (object);
+	return;
+}
+
+static void
+proc_watcher (GPid pid, gint status, gpointer data)
+{
+
+
+}
+
+gboolean
+proc_writes (GIOChannel * channel, GIOCondition condition, gpointer data)
+{
+
+	return TRUE;
+}
+
+void
+process_run (DbusTestTask * task)
+{
+	g_return_if_fail(DBUS_TEST_IS_PROCESS(task));
+	DbusTestProcess * process = DBUS_TEST_PROCESS(task);
+
+	gchar ** argv;
+	argv = g_new0(gchar *, g_list_length(process->priv->parameters) + 2);
+
+	argv[0] = process->priv->executable;
+	int i;
+	for (i = 0; i < g_list_length(process->priv->parameters); i++) {
+		argv[i + 1] = (gchar *)g_list_nth(process->priv->parameters, i)->data;
+	}
+
+	GError * error = NULL;
+	gint proc_stdout;
+	g_spawn_async_with_pipes(g_get_current_dir(),
+	                         argv, /* argv */
+	                         NULL, /* envp */
+	                         G_SPAWN_SEARCH_PATH | G_SPAWN_DO_NOT_REAP_CHILD, /* flags */
+	                         NULL, /* child setup func */
+	                         NULL, /* child setup data */
+							 &(process->priv->pid), /* PID */
+	                         NULL, /* stdin */
+	                         &proc_stdout, /* stdout */
+	                         NULL, /* stderr */
+	                         &error); /* error */
+	g_free(argv);
+
+	if (error != NULL) {
+		g_warning("Unable to start process '%s': %s", process->priv->executable, error->message);
+		/* TODO: Signal finished */
+		return;
+	}
+
+	if (TRUE) {
+		gchar * message = g_strdup_printf("Started with PID: %d", process->priv->pid);
+		dbus_test_task_print(task, message);
+		g_free(message);
+	}
+
+	GIOChannel * iochan = g_io_channel_unix_new(proc_stdout);
+	g_io_channel_set_buffer_size(iochan, 10 * 1024 * 1024); /* 10 MB should be enough for anyone */
+	process->priv->io_watch = g_io_add_watch(iochan,
+	                                         G_IO_IN, /* conditions */
+	                                         proc_writes, /* func */
+	                                         process); /* func data */
+
+	process->priv->watcher = g_child_watch_add(process->priv->pid, proc_watcher, process);
+
 	return;
 }
