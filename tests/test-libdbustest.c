54c98100186c7a014b2b4448aa165a56746c35ea
Ted Gould <ted@gould.cx>
2012-04-19 23:00:22 -0500

    Setting up a wait task and one that grabs the name

--- tests/test-libdbustest.c
+++ tests/test-libdbustest.c
@@ -40,12 +40,45 @@ test_task_start (void)
 	return;
 }
 
+void
+test_task_wait (void)
+{
+	DbusTestService * service = dbus_test_service_new(NULL);
+	g_assert(service != NULL);
+
+	dbus_test_service_set_conf_file(service, SESSION_CONF);
+
+	DbusTestTask * task = dbus_test_task_new();
+	g_assert(task != NULL);
+
+	dbus_test_task_set_wait_for(task, "org.test.name");
+
+	dbus_test_service_add_task(service, task);
+
+	DbusTestProcess * proc = dbus_test_process_new(GETNAME_PATH);
+	g_assert(proc != NULL);
+	g_assert(DBUS_TEST_IS_TASK(proc));
+	dbus_test_process_append_param(proc, "org.test.name");
+
+	dbus_test_service_add_task_with_priority(service, DBUS_TEST_TASK(proc), DBUS_TEST_SERVICE_PRIORITY_LAST);
+
+	dbus_test_service_start_tasks(service);
+
+	g_assert(dbus_test_task_get_state(task) == DBUS_TEST_TASK_STATE_FINISHED);
+
+	g_object_unref(task);
+	g_object_unref(service);
+
+	return;
+}
+
 /* Build our test suite */
 void
 test_libdbustest_suite (void)
 {
 	g_test_add_func ("/libdbustest/env_var",    test_env_var);
 	g_test_add_func ("/libdbustest/task_start", test_task_start);
+	g_test_add_func ("/libdbustest/task_wait",  test_task_wait);
 
 	return;
 }
