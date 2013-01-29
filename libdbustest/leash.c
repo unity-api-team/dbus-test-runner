89632bb902592f9490654f124cd586e95774e18d
Ted Gould <ted@gould.cx>
2013-01-29 15:42:13 +0000

    Fleshing this guy out

--- libdbustest/leash.c
+++ libdbustest/leash.c
@@ -1,6 +1,40 @@
 #include <glib.h>
 
 GMainLoop * mainloop = NULL;
+gulong timer = 0;
+pid_t victim = 0;
+
+static void sigterm_graceful_exit (int signal);
+
+static gboolean
+destroy_everyone (gpointer user_data)
+{
+	if (victim != 0) {
+		kill(victim, SIGTERM);
+	}
+
+	sigterm_graceful_exit(0);
+	return;
+}
+
+static void
+restart_handler (void)
+{
+	if (timer != 0) {
+		g_source_remove(timer);
+	}
+
+	timer = g_timeout_add_seconds(30, destroy_everyone, NULL);
+
+	return;
+}
+
+static void
+sighup_dont_die (int signal)
+{
+	restart_handler();
+	return;
+}
 
 static void
 sigterm_graceful_exit (int signal)
@@ -19,6 +53,12 @@ main (int argc, char * argv[])
 
 	g_type_init();
 
+	signal(SIGTERM, sigterm_graceful_exit);
+	signal(SIGHUP, sighup_dont_die);
+
+	victim = atoi(argv[1]);
+
+	restart_handler();
 
 	mainloop = g_main_loop_new(NULL, FALSE);
 	g_main_loop_run(mainloop);
