0c5bf05b586d205bc3e13b63136192866dad5361
Ted Gould <ted@gould.cx>
2013-10-30 15:23:46 -0700

    Use a common bus and make sure to build the proper array

--- libdbustest/dbus-mock.c
+++ libdbustest/dbus-mock.c
@@ -258,7 +258,7 @@ install_object (DbusTestDbusMock * mock, DbusTestDbusMockObject * object)
 	GVariant * properties = NULL;
 	GVariant * methods = NULL;
 
-	g_return_val_if_fail(object->proxy != NULL, FALSE);
+	g_return_val_if_fail(mock->priv->proxy != NULL, FALSE);
 
 	if (object->properties->len > 0) {
 		GVariantBuilder property_builder;
@@ -305,7 +305,7 @@ install_object (DbusTestDbusMock * mock, DbusTestDbusMockObject * object)
 		return add_object;
 	}
 
-	object->proxy = dbus_mock_iface_org_freedesktop_dbus_mock_proxy_new_for_bus_sync(G_BUS_TYPE_SESSION,
+	object->proxy = dbus_mock_iface_org_freedesktop_dbus_mock_proxy_new_sync(mock->priv->bus,
 		G_DBUS_PROXY_FLAGS_DO_NOT_LOAD_PROPERTIES | G_DBUS_PROXY_FLAGS_DO_NOT_AUTO_START,
 		mock->priv->name,
 		object->object_path, /* path */
@@ -354,7 +354,7 @@ run (DbusTestTask * task)
 	/**** Initialize the DBus Mock instance ****/
 
 	/* Zero, Setup the proxy */
-	self->priv->proxy = dbus_mock_iface_org_freedesktop_dbus_mock_proxy_new_for_bus_sync(G_BUS_TYPE_SESSION,
+	self->priv->proxy = dbus_mock_iface_org_freedesktop_dbus_mock_proxy_new_sync(self->priv->bus,
 		G_DBUS_PROXY_FLAGS_DO_NOT_LOAD_PROPERTIES | G_DBUS_PROXY_FLAGS_DO_NOT_AUTO_START,
 		self->priv->name,
 		"/", /* path */
@@ -874,27 +874,29 @@ dbus_test_dbus_mock_object_update_property (DbusTestDbusMock * mock, DbusTestDbu
 	}
 
 	/* Send the update to Dbusmock */
-	GError * error = NULL;
-	g_dbus_connection_call_sync(mock->priv->bus,
-		mock->priv->name,
-		obj->object_path,
-		"org.freedesktop.DBus.Properties",
-		"Set",
-		g_variant_new("(ssv)",
-			obj->interface,
-			name,
-			value),
-		NULL, /* return */
-		G_DBUS_CALL_FLAGS_NO_AUTO_START,
-		-1, /* timeout */
-		NULL, /* TODO: cancel */
-		&error); /* TODO: error */
+	if (is_running(mock)) {
+		GError * error = NULL;
+		g_dbus_connection_call_sync(mock->priv->bus,
+			mock->priv->name,
+			obj->object_path,
+			"org.freedesktop.DBus.Properties",
+			"Set",
+			g_variant_new("(ssv)",
+				obj->interface,
+				name,
+				value),
+			NULL, /* return */
+			G_DBUS_CALL_FLAGS_NO_AUTO_START,
+			-1, /* timeout */
+			NULL, /* TODO: cancel */
+			&error); /* TODO: error */
 
-	if (error != NULL) {
-		g_warning("Unable to update property: %s", error->message);
-		g_error_free(error);
-		g_variant_unref(value);
-		return FALSE;
+		if (error != NULL) {
+			g_warning("Unable to update property: %s", error->message);
+			g_error_free(error);
+			g_variant_unref(value);
+			return FALSE;
+		}
 	}
 
 	/* It's updated, let's cache */
@@ -904,27 +906,44 @@ dbus_test_dbus_mock_object_update_property (DbusTestDbusMock * mock, DbusTestDbu
 	if (!signal) {
 		return TRUE;
 	}
+
+	if (!is_running(mock)) {
+		g_debug("Not running, don't update");
+		return FALSE;
+	}
+
+	g_return_val_if_fail(obj->proxy != NULL, FALSE);
 	
 	GVariantBuilder builder;
-	g_variant_builder_init(&builder, G_VARIANT_TYPE_TUPLE);
+	g_variant_builder_init(&builder, G_VARIANT_TYPE_ARRAY);
 
-	g_variant_builder_add_value(&builder, g_variant_new_string(obj->interface));
+	g_variant_builder_add_value(&builder, g_variant_new_variant(g_variant_new_string(obj->interface)));
+	g_variant_builder_open(&builder, G_VARIANT_TYPE_VARIANT);
 	g_variant_builder_open(&builder, G_VARIANT_TYPE_DICT_ENTRY);
 	g_variant_builder_add_value(&builder, g_variant_new_string(name));
 	g_variant_builder_add_value(&builder, g_variant_new_variant(value));
 	g_variant_builder_close(&builder);
-	g_variant_builder_add_value(&builder, g_variant_new_array(G_VARIANT_TYPE_STRING, NULL, 0));
+	g_variant_builder_close(&builder);
+	g_variant_builder_add_value(&builder, g_variant_new_variant(g_variant_new_array(G_VARIANT_TYPE_STRING, NULL, 0)));
 
 	/* Signal if we want to here */
-	return dbus_mock_iface_org_freedesktop_dbus_mock_call_emit_signal_sync(
+	GError * error = NULL;
+	gboolean emit_status = dbus_mock_iface_org_freedesktop_dbus_mock_call_emit_signal_sync(
 		obj->proxy,
 		"org.freedesktop.Properties",
 		"PropertiesChanged",
 		"sa{sv}as",
 		g_variant_builder_end(&builder),
 		NULL, /* TODO: Cancelable */
-		NULL /* TODO: Error */
+		&error /* TODO: Error */
 	);
+
+	if (error != NULL) {
+		g_warning("Unable to emit changed signal: %s", error->message);
+		g_error_free(error);
+	}
+
+	return emit_status;
 }
 
 /**
